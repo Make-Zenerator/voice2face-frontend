0ceed572d09b6981bc6e4e597b6b32823fd2183e
carboxaminoo <carboxaminoo@daum.net>
2024-03-11 18:29:39 +0900

    Design : Change logo - #5

--- mz_v1_front/src/routes/login/PlaceholderImage.svelte
+++ mz_v1_front/src/routes/login/PlaceholderImage.svelte
@@ -2,14 +2,15 @@
     let className = "";
     export { className as class };
     export let style;
-
-    export let targetPath = "/";
-  function handleClick() {
-    goto(targetPath); // 버튼 클릭 시 targetPath로 이동
-  }
+    export let src = 'logo/login_image.webp'
+  //   export let targetPath = "/";
+  // function handleClick() {
+  //   goto(targetPath); // 버튼 클릭 시 targetPath로 이동
+  // }
   </script>
   <img
     class="{'placeholder-image ' + className}"
     style="{'border-radius: 24px;padding: 10px; display: flex; flex-direction: row; gap: 10px; align-items: center; justify-content: center; align-self: stretch;flex: 1;height: auto; position: relative; overflow: visible;' + style}"
-    src="../../join/placeholder-image.svg"
+    src={src}
+    alt="login_image"
   />
